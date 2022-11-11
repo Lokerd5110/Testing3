614ac7fcace886e59f9369159cc60ed90f0b8624
lokerd <lokerd5110@yandex.ru>
2022-11-11 14:48:36 +0300

    .

---
+++ Assets/sceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+public class sceneChanger: MonoBehaviour {
+    public string SceneName;
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
